878deee4a720404b117fd962a3b5993763f0bb2f
Jagoda Parzyjagła <jagodaparzyjagla@gmail.com>
2021-07-01 13:01:05 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Jagoda");
+        System.out.println(27);
+        System.out.println("P");
+    }
+
+}
