218aa1c9ce72dac4c6a9fb267538386fa5c48f29
Jagoda Parzyjagła <jagodaparzyjagla@gmail.com>
2021-07-02 16:17:31 +0200

    Initial changes

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String firstName = "Marshall";
+        String secondName = "Bruce";
+        String lastName = "Mathers";
+        char witchOne = '3';
+        int age = 48;
+        String kindOfMusic = "rap";
+
+        System.out.println(firstName);
+        System.out.println(secondName);
+        System.out.println(lastName);
+        System.out.println(witchOne);
+        System.out.println(age);
+        System.out.println(kindOfMusic);
+    }
+}
