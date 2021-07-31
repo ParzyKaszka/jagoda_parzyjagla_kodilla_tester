85e2df624dad65da218b1f50e3ef7d3fc3cbc205
Jagoda Parzyjagła <jagodaparzyjagla@gmail.com>
2021-07-31 19:43:42 +0200

    Initital changes

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,20 @@
+public class Application {
+        String name;
+        double age;
+        double height;
+
+    public Application(String name, double age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    public static void main(String[] args) {
+        Application czlowiek = new Application("Adam", 40.5, 178);
+
+        if ( czlowiek.age > 30 && czlowiek.height > 160) {
+            System.out.println("User is older than 30 and higher than 160cm");
+        } else {
+            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+        }
+        }
+    }
